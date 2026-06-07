a55aabb4a5f5ba35a125013e2704b3adcc9b0185
Anmol Kumar <anmolkroe@gmail.com>
2026-06-07 06:33:24 +0530

    add note retrieval endpoints; implement getMyNotes and getSingleNotes functions in notes.controller.js

--- Backend/routes/generate.route.js
+++ Backend/routes/generate.route.js
@@ -1,7 +1,7 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
 import { generateNotes } from "../controllers/generate.controller.js"
-// import { getMyNotes, getSingleNotes } from "../controllers/notes.controller.js"
+import { getMyNotes, getSingleNotes } from "../controllers/notes.controller.js"
 
 
 
@@ -9,7 +9,7 @@ const notesRouter = express.Router()
 
 
 notesRouter.post("/generate-notes",isAuth,generateNotes)
-// notesRouter.get("/getnotes", isAuth,getMyNotes)
-// notesRouter.get("/:id" , isAuth , getSingleNotes)
+notesRouter.get("/getnotes", isAuth,getMyNotes)
+notesRouter.get("/:id" , isAuth , getSingleNotes)
 
 export default notesRouter
